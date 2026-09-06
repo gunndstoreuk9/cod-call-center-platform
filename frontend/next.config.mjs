adc82898dbd40af55a6563be35cea9046e79bd50
belhaj <gunndstoreuk9@gmail.com>
2026-09-06 06:58:20 +0100

    Proxy API through frontend for session cookies

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  poweredByHeader: false
+  poweredByHeader: false,
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: `${process.env.BACKEND_ORIGIN}/api/v1/:path*`,
+      },
+    ]
+  },
 }
+
 export default nextConfig
